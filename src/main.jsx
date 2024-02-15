ec9e3def9271b087bedf75c25e1311311385afb6
Andrey <andrejlapin4594@gmail.com>
2024-02-15 22:56:25 +0500

    Подправил main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import AppRouter from './routing/AppRouter.jsx'
 
@@ -17,5 +17,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <QueryClientProvider client={queryClient} >
           <AppRouter/>
       </QueryClientProvider>
-    </BrowserRouter>,
+  </BrowserRouter>,
 )
